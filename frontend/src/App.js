fee5a13d0f4a32b320fa35a3d4d82caf24a0ec14
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-10-11 00:12:52 +0300

    Add Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Container} from 'react-bootstrap';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+        <Header />
+        <Container>
+            <main className='py-5'>
+                <HomeScreen />
+            </main>
+
+        </Container>
+
+        <Footer />
     </>
   );
 }
