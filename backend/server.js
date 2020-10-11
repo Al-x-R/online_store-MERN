b94c9259ba8ad06e5b482b7c34bf5bc4f0770201
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-10-11 21:33:39 +0300

    Add connection DB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const http = require('http');
 const app = require('./app');
 const dotenv = require('dotenv');
+const colors = require('colors')
 const server = http.createServer(app);
+const connectDB = require('./config/db');
 
 dotenv.config();
+
+connectDB();
 
 const port = process.env.POPT ?? 5000;
 
 server.listen(port, () => {
-    console.log(`server is running in ${process.env.NODE_ENV} on http://localhost:${port}`);
+    console.log(`server is running in ${process.env.NODE_ENV} on http://localhost:${port}`.yellow.underline.bold);
 });
