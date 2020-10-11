215d1605c6288d808a9520d9cffc75b4af26b6f6
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-10-11 14:32:26 +0300

    Add ProductScreen

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Row, Col} from 'react-bootstrap';
+import products from '../products';
+import Product from '../components/Product';
+
+const ProductScreen = () => {
+
+    return (
+        <>
+            <h2>Product</h2>
+        </>
+    );
+};
+
+export default ProductScreen;
