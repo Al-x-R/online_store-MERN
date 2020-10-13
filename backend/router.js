a4de77d6d56c362b98a2e1b6457fcbb9685ab076
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-10-13 22:00:41 +0300

    Add error middleware

--- backend/router.js
+++ backend/router.js
@@ -1,7 +1,10 @@
 const {Router} = require('express');
 const router = Router();
 const productRouter = require('./routes/productRouter');
+const {errorHandler, notFound} = require('./middleware/errorMiddleware');
 
 router.use('/products', productRouter);
+router.use(notFound);
+router.use(errorHandler);
 
 module.exports = router;
--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -5,20 +5,22 @@ const asyncHandler = require('express-async-handler');
 const productRouter = express.Router();
 
 productRouter.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({});
+        const products = await Product.find({});
 
-    res.json(products);
-}));
+        res.json(products);
+    }),
+);
 
 productRouter.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
+        const product = await Product.findById(req.params.id);
 
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404).json({message: 'Product not found'});
-    }
-}));
-
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404);
+            throw new Error('Product not found');
+        }
+    }),
+);
 
 module.exports = productRouter;
