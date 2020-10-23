93e3993b1e691b4aca12a2bbc381d81a86ebbe00
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-10-23 11:21:26 +0300

    Add redux store

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,21 @@
+import ACTION_TYPE from './types';
+import axios from 'axios';
+
+export const listProducts = () => async (dispatch) => {
+    try {
+        dispatch({type: ACTION_TYPE.PRODUCT_LIST_REQUEST});
+        const {data} = await axios.get('/api/products');
+        // console.log(data);
+        dispatch({
+            type: ACTION_TYPE.PRODUCT_LIST_SUCCESS,
+            payload: data,
+        });
+    } catch (e) {
+        dispatch({
+            type: ACTION_TYPE.PRODUCT_LIST_ERROR,
+            payload: e.response && e.response.data.message
+                ? e.response.data.message
+                : e.message,
+        });
+    }
+};
---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,31 @@
+import ACTION_TYPE from '../actions/types';
+
+const initialState = {
+    products: [],
+    error: null,
+    loading: false,
+};
+
+export const productListReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ACTION_TYPE.PRODUCT_LIST_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+        case ACTION_TYPE.PRODUCT_LIST_SUCCESS:
+            return {
+                ...state,
+                products: action.payload,
+                loading: false,
+            };
+        case ACTION_TYPE.PRODUCT_LIST_ERROR:
+            return {
+                ...state,
+                error: action.payload,
+                loading: false,
+            };
+        default:
+            return state;
+    }
+};
