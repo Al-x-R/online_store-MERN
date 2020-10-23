e30baff8ff04c4604c340062d698499f4dc2417a
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-10-23 12:06:22 +0300

    Add Loader and Error components

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -6,7 +6,7 @@ const productRouter = express.Router();
 
 productRouter.get('/', asyncHandler(async (req, res) => {
         const products = await Product.find({});
-
+        // throw new Error('Some error');
         res.json(products);
     }),
 );
